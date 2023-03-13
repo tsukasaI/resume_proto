bc180cdf77bd2b2564990ac90de0a846d2ea1237
tsukasa.inoue <tsukasa.inoue@ariseanalytics.com>
2023-03-13 09:40:37 +0900

    devide axios interface

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { QiitaUserItems } from "../model/qiita";
 
-export const getQiitaRequest = async (
+export const getQiitaRequest = (
   url: string
 ): Promise<QiitaUserItems[] | undefined> => {
+  const result = getRequest<QiitaUserItems[]>(url);
+  return result;
+};
+
+// これinterface化する
+export const getRequest = async <T>(url: string): Promise<T | undefined> => {
   try {
-    const response = (await axios.get(url)) as QiitaUserItems[];
+    const response = (await axios.get(url)) as T;
     return response;
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    console.error(error);
     return;
   }
 };
